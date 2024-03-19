aaeecb696b46fab03b5d3c2c9605ed2bd0471fbf
Ömer Hodo <omerhodo@gmail.com>
2024-03-19 09:00:14 +0300

    add button component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,20 @@
 import Header from '@components/Header';
+import Button from '@components/Button';
 
 const Home = () => {
   return (
     <div className="home">
       <Header />
+      <div className="home__new-message">
+        <Button
+          text="New Message"
+          radius="10"
+          bgColor="green"
+          borderColor="green"
+          textColor="white"
+          fontSize="20px"
+        />
+      </div>
     </div>
   );
 };
