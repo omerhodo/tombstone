e153a4202849aa0e61c2e713c5eb0cb09d1d5ae8
Ömer Hodo <omerhodo@gmail.com>
2024-03-25 23:35:23 +0300

    update new message button

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -2,6 +2,8 @@ import Navbar from '@layouts/Navbar';
 import Header from '@layouts/Header';
 import Footer from '@layouts/Footer';
 
+import Button from '@/components/Button';
+
 import '@styles/layouts/main-layouts.scss';
 
 const MainLayout = ({ children }) => {
@@ -9,6 +11,13 @@ const MainLayout = ({ children }) => {
     <div className="main-layouts">
       <Navbar />
       <Header />
+      <Button
+        className="container mb-60 is-cursor-pointer"
+        text="Yeni Mesaj"
+        fontSize="20px"
+        fontWeight="400"
+        width="600px"
+      />
       {children}
       <Footer />
     </div>
