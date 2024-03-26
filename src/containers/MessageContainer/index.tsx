587761d4a8c1acfdd466c345efbd379f036f1d42
Ömer Hodo <omerhodo@gmail.com>
2024-03-26 18:07:22 +0300

    update

--- src/containers/MessageContainer/index.tsx
+++ src/containers/MessageContainer/index.tsx
@@ -1,8 +1,17 @@
+import { useState, useEffect } from 'react';
+import { getData } from '@/firebase';
 import MessageCard from '@components/MessageCard';
 
 import '@styles/containers/message-container.scss';
 
 const MessageContainer = () => {
+  const [messages, setMessages] = useState<any[]>([]);
+
+  useEffect(() => {
+    getData('messages').then(setMessages);
+    console.log(messages);
+  }, []);
+
   return (
     <>
       <div className="container message-container">
--- src/firebase/index.tsx
+++ src/firebase/index.tsx
@@ -1,5 +1,6 @@
 import { signIn, signUp, signOutUser, authUser } from './auth';
 import { sendData } from './sendData';
+import { getData } from './getData';
 import { auth, db } from './firebase-config';
 
-export { auth, signIn, signUp, signOutUser, authUser, db, sendData };
+export { auth, signIn, signUp, signOutUser, authUser, db, sendData, getData };
