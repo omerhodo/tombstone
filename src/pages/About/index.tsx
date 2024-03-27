54a5d78d485e6a648e4216e26f4f52318a253006
Ömer Hodo <omerhodo@gmail.com>
2024-03-27 12:39:59 +0300

    .

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -3,7 +3,7 @@ const About = () => {
     <div className="about container">
       <h1 className="about--title">Sevgili Misafir...</h1>
       <p className="about--description">
-        Burada yazılan yazıları benim okuyup okuyamağımdan emin değiliz.
+        Burada yazılan yazıları benim okuyup okuyamadığımdan emin değiliz.
         <br />
         Sizden ricam, buraya uğradığınızda, eğer canınız isterse benimle ilgili
         anılarınızı, düşüncelerinizi, belki de benimle yaşadığınız komik anları
