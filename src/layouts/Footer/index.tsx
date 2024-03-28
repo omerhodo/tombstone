d72d901eda4dd1f93e1f5544ffad905e687062db
Ömer Hodo <omerhodo@gmail.com>
2024-03-28 04:26:50 +0300

    update

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -1,11 +1,14 @@
+import { useTranslation } from 'react-i18next';
 import AuthForm from '@components/AuthForm';
 
 import '@styles/layouts/footer.scss';
 
 const Footer = () => {
+  const { t } = useTranslation('general');
   return (
     <footer className="footer">
       <AuthForm />
+      <p className="footer__quote">{t('footerQuote')}</p>
     </footer>
   );
 };
