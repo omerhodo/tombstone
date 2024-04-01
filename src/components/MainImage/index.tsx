c3ae5c57a0c15de172d735c64b39a0fbc931d385
Ömer Hodo <omerhodo@gmail.com>
2024-04-01 09:41:32 +0300

    update main image

--- src/components/MainImage/index.tsx
+++ src/components/MainImage/index.tsx
@@ -1,11 +1,13 @@
-import MainImageRef from '@/assets/images/main/main-image.jpg';
-
 import '@styles/layouts/header.scss';
 
 const MainImage = () => {
   return (
     <div className="header">
-      <img className="header__image" src={MainImageRef} alt="MainImage" />
+      <img
+        src="https://lh3.googleusercontent.com/pw/AP1GczN89L5EWwW-LK_7oxA_q2TwTHoJazQUKOtC1e5HWj2x_GgRZW04EUIc7_eHxuF0ujjNfZs8dq4SX5Q_rl0DsogILUT5RR-yw9gCR1O4WyTXMd721D4M6Iu0KKSJ-aUW6qR-kzS2Ligc0kocjlzuTLDfuw=w1024-h564-s-no-gm"
+        alt="Header Image"
+        className="header--image"
+      />
     </div>
   );
 };
