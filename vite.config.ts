b52a08b926fd02068f1f51d9aa4946ceb917cc7a
Ömer Hodo <omerhodo@gmail.com>
2026-03-03 17:59:42 +0300

    Refactor code structure for improved readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern',
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
